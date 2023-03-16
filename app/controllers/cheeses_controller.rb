935459b8bc32f352629d695acf8b0f7f63b48410
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-03-16 17:57:27 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,14 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
-  end
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    
   end
 
 end
